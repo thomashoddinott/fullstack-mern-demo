a2018ad0dbc12e7e19be20c63bbe23ef0d1025b2
Thomas Hoddinott <thomashoddinott477@gmail.com>
2025-11-06 14:16:44 +0100

    init routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,23 @@
-import Navbar from "./components/Navbar";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./Layout";
+
+const routes = [
+  {
+    path: "/",
+    element: <Layout/>,
+    children: [
+      {
+        path: "/",
+        // element: <ArticlesListPage />,
+      },
+    ],
+  },
+];
+
+const router = createBrowserRouter(routes);
 
 function App() {
-  return <Navbar />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
