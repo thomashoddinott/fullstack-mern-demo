4491ab14ba49871565a64a27fa497599de6e73ac
Thomas Hoddinott <thomashoddinott477@gmail.com>
2025-12-16 11:35:08 +0100

    try to resolve GH actions with updated `setup.js`

--- frontend/src/test/setup.js
+++ frontend/src/test/setup.js
@@ -1,6 +1,14 @@
+/* eslint-disable no-undef */
 import { expect, afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import * as matchers from '@testing-library/jest-dom/matchers';
+
+// Polyfill for jsdom
+if (typeof global.TextEncoder === 'undefined') {
+  const { TextEncoder, TextDecoder } = require('util');
+  global.TextEncoder = TextEncoder;
+  global.TextDecoder = TextDecoder;
+}
 
 expect.extend(matchers);
 
