b61b701c34bf9cc2b6b4ca5c17dea5fa3e6cf3f1
Thomas Hoddinott <thomashoddinott477@gmail.com>
2025-12-25 12:52:59 +0000

    allow exports for unit testing

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -26,6 +26,10 @@ export default defineConfig([
     },
     rules: {
       "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
     },
   },
 ])
